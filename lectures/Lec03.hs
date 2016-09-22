c40178005419fcdfe1b62d26661189d02de76118
Conor McBride <conor@strictlypositive.org>
2016-09-22 18:41:55 +0100

    lec 3 ready

---
+++ lectures/Lec03.hs
@@ -0,0 +1,33 @@
+module Lec03 where
+
+-- 3 Defining Functions
+
+
+-- declarations versus definitions
+--   a declaration introduces new types or values
+--   a definition gives a new way to compute existing values
+
+
+-- definition by equation (Fred)
+--   without parameters
+--   with parameters
+--   using conditional expression (e.g., not)
+
+
+-- replacing conditionals on the right by guards on the left (Conor)
+--   e.g., greatest common divisor (gcd is already a thing, so rename)
+--   also, not via guards
+
+
+-- pattern matching (Fred)
+--   e.g., not not again? (underscore patterns)
+--   e.g., maybeApply, maybe?  (:info Maybe, then use shplit,
+--     with underscore expressions)
+
+
+-- pattern matching on lists (Conor)
+--   e.g., append
+--   (chant and be happy)
+--   e.g., reverse (the slow way)
+--   e.g., sawPrefixOff
+
