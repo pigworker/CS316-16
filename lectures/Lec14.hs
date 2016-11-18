1f94e8b843f93316947238131a30df5fd590435a
Robert Atkey <bob.atkey@gmail.com>
2016-11-18 14:35:59 +0000

    Bit more detail in Lec14

--- lectures/Lec14.hs
+++ lectures/Lec14.hs
@@ -143,7 +143,8 @@ sepBy separator thing =
 
   -- sepBy (isChar ',') char
 
-{-
+{- Parsing comma separated values:
+
      λ> runParser (sepBy (isChar ',') char) "1,2,3"
      ["123"]
      λ> runParser (sepBy (isChar ',') char) "1,2,,3"
@@ -189,16 +190,25 @@ data Expr
   deriving Show
 
 {-
+   A grammar for expressions:
+
     E ::= n
         | E '+' E
 
-    1+1+1
+   Unfortunately, this grammar is ambiguous. The string:
 
-    (1+1)+1
-    1+(1+1)
--}
+      1+1+1
 
-{- Try to parse via left recursion -}
+   Can be parsed in two different ways.
+
+      (1+1)+1
+      1+(1+1)
+
+   Another problem is that the grammar uses left recursion. The second
+   case for 'E' immediately mentions 'E' without moving through the
+   input. This means that if we try to turn this grammar into a parser
+   directly, we will fail: -}
+
 expr_v1 :: Parser Expr
 expr_v1 =  Number                  <$> (const 1 <$> some (satisfies isDigit))
        <|> (\e1 _ e2 -> Add e1 e2) <$> expr_v1 <*> isChar '+' <*> expr_v1
@@ -208,10 +218,16 @@ expr_v1 =  Number                  <$> (const 1 <$> some (satisfies isDigit))
      [Add (Number 1) (Number 1)*** Exception: stack overflow
 -}
 
+{- One way to fix this is to just demand that there are always
+   parentheses around every expression that isn't a number, using the
+   'parens' function. This means we are parsing the following grammar:
 
-{- Fully paren'd version -}
+     E ::= n
+         | '(' E '+' E ')'
+-}
+
 expr_v2 :: Parser Expr
-expr_v2 = Number                  <$> (const 1 <$> some (satisfies isDigit))
+expr_v2 = Number                           <$> (const 1 <$> some (satisfies isDigit))
        <|> parens ((\e1 _ e2 -> Add e1 e2) <$> expr_v2 <*> isChar '+' <*> expr_v2)
 
 {-
@@ -223,32 +239,42 @@ expr_v2 = Number                  <$> (const 1 <$> some (satisfies isDigit))
      []
 -}
 
-{- E := B
-      | B '+' E
+{- Putting in all those parentheses is annoying, so we can refactor the
+   grammar into 'E'xpressions and 'B'ase expressions. This resolves
+   the ambiguity, and the left recursion problems:
 
-   B := n
-      | '(' E ')' -}
+     E ::= B
+         | B '+' E
 
-{- Version with big expressions, little expressions -}
+     B ::= n
+         | '(' E ')'
+-}
+
 expr_v3 :: Parser Expr
 expr_v3 = base_v3
        <|> Add <$> base_v3 <* isChar '+' <*> expr_v3
 
--- (<*>) :: Parser (a -> b) -> Parser a -> Parser b
--- (<*)  :: Parser a -> Parser b -> Parser a
+{- In this definition, I have used the function '<*' to throw away the
+   results of parsing the '+' symbol. The '<*' function has the
+   following type, compared to the more normal '<*>' function:
+
+       (<*)  :: Parser a -> Parser b -> Parser a
+       (<*>) :: Parser (a -> b) -> Parser a -> Parser b
+-}
 
 base_v3 = Number <$> (const 1 <$> some (satisfies isDigit))
        <|> parens (expr_v3)
 
-{-
-     λ> runParser expr_v3 "(1+1+1)"
-     [Add (Number 1) (Add (Number 1) (Number 1))]
-     λ> runParser expr_v3 "1+1+1"
-     [Add (Number 1) (Add (Number 1) (Number 1))]
-     λ> runParser expr_v3 "(1+1)+1"
-     [Add (Add (Number 1) (Number 1)) (Number 1)]
-     λ> runParser expr_v3 "(1+1)+1"
-     [Add (Add (Number 1) (Number 1)) (Number 1)]
+{- Example uses:
+
+       λ> runParser expr_v3 "(1+1+1)"
+       [Add (Number 1) (Add (Number 1) (Number 1))]
+       λ> runParser expr_v3 "1+1+1"
+       [Add (Number 1) (Add (Number 1) (Number 1))]
+       λ> runParser expr_v3 "(1+1)+1"
+       [Add (Add (Number 1) (Number 1)) (Number 1)]
+       λ> runParser expr_v3 "(1+1)+1"
+       [Add (Add (Number 1) (Number 1)) (Number 1)]
 -}
 
 
